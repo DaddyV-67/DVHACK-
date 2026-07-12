89b9da864ca5562daa5770ee2eb359c1bc656be5
DaddyV-67 <victor.hill.kairangi@gmail.com>
2026-07-13 10:41:06 +1200

    Add files via upload

---
+++ multitool.py
@@ -0,0 +1,300 @@
+import subprocess
+import os
+import sys
+import time
+import random
+
+os.system('clear')
+
+
+
+Banner = random.randint(1, 7)
+
+
+RED = '\033[31m'
+
+
+if Banner == 1:
+
+    print('')
+    print('')
+    print('')
+    print('\033[35m██████╗  ██╗   ██╗    ██╗  ██╗ █████╗  ██████╗██╗  ██╗')
+    print('███╔══██╗██║   ██║    ██║  ██║██╔══██╗██╔════╝██║ ██╔╝')
+    print('███║  ██║██║   ██║    ███████║███████║██║     █████╔╝ ')
+    print('███║  ██║╚██╗ ██╔╝    ██╔══██║██╔══██║██║     ██╔═██╗ ')
+    print('███████╔╝ ╚████╔╝     ██║  ██║██║  ██║╚██████╗██║  ██╗')
+    print('╚══════╝   ╚═══╝      ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    Multitool V1\033[0m')
+    print('')                                            
+    print('')
+    print('')
+
+elif Banner == 2:
+    print('')
+    print('')
+    print('')
+    print('')
+    print("\033[36m") 
+
+    print('                                           `:oDFo:`          ')
+    print('                                       ./ymM0dayMmy/.                       ')
+    print('                                    -+dHJ5aGFyZGVyIQ==+-                    ')
+    print('                                `:sm⏣~~Destroy.No.Data~~s:`                   ')
+    print('                             -+h2~~Maintain.No.Persistence~~h+-                ')
+    print('                         `:odNo2~~Above.All.Else.Do.No.Harm~~Ndo:`              ')
+    print('                     ./etc/shadow.0days-Data,%20OR%201=1--.No.0MN8./.           ')
+    print('                   -++SecKCoin++e.AMd`       `.-://///+hbove.913.ElsMNh+-        ')
+    print('                  -~/.ssh/id_rsa.Des-                  `htN01UserWroteMe!-        ')
+    print('                  :dopeAW.No<nano>o                     :is:TЯiKC.sudo-.A:        ')
+    print('                  :were.all.alike,`                     :The.PFYroy.No.D7:         ')
+    print('                  :PLACEDRINKHERE!:                      yxp_cmdshell.Ab0:         ')
+    print('                  :msf>exploit -j.                       :Ns.BOB&ALICEes7:         ')
+    print('                  :---srwxrwx:-.`                        `MS146.52.No.Per:         ')
+    print('                  :<script>.Ac816/                        sENbove3101.404:         ')
+    print('                  :NT_AUTHORITY.Do                        `T:/shSYSTEM-.N:         ')
+    print('                  :09.14.2011.raid                       /STFU|wall.No.Pr:       ')
+    print('                  :hevnsntSurb025N.                      dNVRGOING2GIVUUP:       ')
+    print('                  :#OUTHOUSE-  -s:                       /corykennedyData:       ')
+    print('                  :$nmap -oS                              SSo.6178306Ence:      ')
+    print('                  :Awsm.da:                            /shMTl#beats3o.No.:       ')
+    print('                  :Ring0:                             `dDestRoyREXKC3ta/M:        ')
+    print('                  :23d:                               sSETEC.ASTRONOMYist:          ')
+    print('                   /-                        /yo-    .ence.N:(){ :|: & };:      ')
+    print('                                             `:Shall.We.Play.A.Game?tron/        ')
+    print('                                             ```-ooy.if1ghtf0r+ehUser5`          ')
+    print('                                           ..th3.H1V3.U2VjRFNN.jMh+.`            ')
+    print('                                          `MjM~~WE.ARE.se~~MMjMs                ')
+    print('                                          +~KANSAS.CITYs~-_`                    ')
+    print('                                            J~HAKCERS~./.`                      ')
+    print('                                            .esc:wq!:`                          ')
+    print('                                             +++ATH`                           \033[0m')
+    print('')
+    print('')
+    print('')
+    print('                                     \033[38;2;255;165;0mThanks For Using DVHACK')
+    print('')
+    print("\033[0m")
+
+
+if Banner == 3:
+
+    print('\033[31m')                                                                                                  
+    print('')                                                                                                   
+    print('')                                                                                                   
+    print('')                                                                                                                                                                                                                                                                                           
+    print('                       .........                           ..........              ')                  
+    print('               .........:----:..........            .........:----:.........            ')                 
+    print('             ....=****************:....           ....-****************-...             ')                
+    print('             ..+**********************-....      ...=**********************=...           ')               
+    print('          ..:***************************=.... ....+**************************+...         ')               
+    print('         ..+******************************-.. ..+******************************=.          ')              
+    print('        .:*********************************+...*#*******************************+...         ')            
+    print('     ...-************************************-#**###**##**************************:.         ')           
+    print('     ...**************************************####*********************************..         ')           
+    print('     ..**************************************######*##*****************************=.         ')           
+    print('     .-**************************************####***********************************...       ')           
+    print('     .=*****************+.           ..=********:  .+**************-  .+************:..       ')           
+    print('     .=*****************+   -+++++=:.    .***###*.  :*************+  .=*************:..         ')         
+    print('     .=*****************+   =*********=.   -*####=  .=************.  :**************:..           ')       
+    print('     .-*****************+   =***********:   :*###*:  .+**********=  .+**************...         ')         
+    print('     .:*****************+   =************:   +####+.  :*****#****  .=*************#*...         ')         
+    print('     ..+****************+   =*****#******+.  -##**#-  .+********.  :******#**###***:.           ')         
+    print('     ...****************+   =************+.  :##*###:   *#*****=  .+********#*#***+.             ')        
+    print('     ...-#**************+   =*************.  :###*#*+.  -******.  =*************##:..            ')        
+    print('        .=**************+   =************+.  -#####**-  .+*#**:  :**#****#***#**#:..             ')        
+    print('        ..+*************+   =***##*****#*-.  =##*##***:  .*##+. .+**************-..              ')        
+    print('        ...=************+   =***********+.  .*########+.  -**. .=#******#**#*#*-.               ')         
+    print('          ..=***********+   =**********-.  :*#####*###*-  .+-  :**##*****##**#:..             ')           
+    print('          ...:**********+   =******+=..  .=**##########*:  .. .+##*****#*****..                ')          
+    print('             ..+********+             .:+****########**#+    .=*#*##**#****-...               ')          
+    print('             ...:********+------==+****##****##########*#*:::=*########***.....               ')           
+    print('               ...-***********#*******#******###****###***####*###*##***....                 ')            
+    print('                  ..-************************########**####***####****:...                  ')             
+    print('                  ....=******##**************############***********-.....                 ')              
+    print('                    ....=********************##########**###*#****-....                ')                  
+    print('                       ...=******************##########*#**##***-....           ')                         
+    print('                          ..=****************###############**:...           ')                            
+    print('                          ....-**************#####*####*****:....       ')                                 
+    print('                            ....:******##****#########****...        ')                                    
+    print('                               ....***#******#########*+...       ')                                       
+    print('                               ......=*******#####*#*-....        ')                                       
+    print('                                    ...-*****####**:....          ')                                       
+    print('                                       ..:***###+:...            ')                                        
+    print('                                         ..:+*=...            ')                                           
+    print('                                         ......... \033[0m')  
+    print('')
+    print('                                 \033[38;2;255;165;0mThanks For Using DVHACK')
+    print('')
+    print('')                                               
+
+if Banner == 4:
+    print('\033[36m\033[36m    __________')
+    print('   <  DVHack  >')
+    print('    ‾‾‾‾‾‾‾‾‾‾')
+    print('         \   ,__,')
+    print('          \  (oo)_____')
+    print('             (__)     )\ ')
+    print('                ||--||  *\033[0m')                                                                                                
+    print('                                    \033[38;2;255;165;0mThanks For Using DVHACK')                                                                                                
+    print('')                                                                                                
+    print('\033[0m')                                                                                                
+                                                                                                                                                                                                                                                                                                       
+if Banner == 5:
+    print('\033[36m                          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+    print('\033[36m                          MMMMMMMMMMM                MMMMMMMMMM')
+    print('\033[36m                          MMMN$                           vMMMM')
+    print('\033[36m                          MMMNl\033[0m      DDDDDDDDDDDDDD       \033[36mJMMMM\033[0m')
+    print('\033[36m                          MMMNl\033[0m      DDDDDDDDDDDDDDDD     \033[36mJMMMM\033[0m')
+    print('\033[36m                          MMMNl\033[0m      DDD          DDDD    \033[36mJMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD           DDD    \033[36mjMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD            DD    \033[36mjMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD            DD    \033[36mjMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD            DD    \033[36mjMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD            DD    \033[36mjMMMM\033[0m')
+    print('\033[36m                          MMMNI\033[0m      DDD           DDD    \033[36mJMMMM\033[0m')
+    print('\033[36m                          MMMMR\033[0m      DDD          DDDD   \033[36m.dMMMM\033[0m')
+    print('\033[36m                          MMMMNm\033[0m     DDDDDDDDDDDDDDDD    \033[36mdMMMMM\033[0m')
+    print('\033[36m                          MMMMMMN \033[0m   DDDDDDDDDDDDDD     \033[36mNMMMMMN\033[0m')
+    print('\033[36m                          MMMMMMMMNe                 JMMMMMNMMM\033[0m')
+    print('\033[36m                          MMMMMMMMMMNm,            eMMMMMNMMNMM\033[0m')
+    print('\033[36m                          MMMMNNMNMMMMMNx        MMMMMMNMMNMMNM\033[0m')
+    print('\033[36m                          MMMMMMMMNMMNMMMMm+..+MMNMMNMNMMNMMNMM\033[0m')
+    print('')
+    print('')
+    print('                                    \033[38;2;255;165;0mThanks For Using DVHACK')
+    print('')
+    print('')
+    print('') 
+
+
+if Banner == 6:
+    print('                      ########                  #')
+    print('                  #################            #')
+    print('               ######################         #')
+    print('              #########################      #')
+    print('            ############################')
+    print('           ##############################')
+    print('           ###############################')
+    print('          ###############################')
+    print('          ##############################')
+    print('                          #    ########   #')
+    print('             \033[31m##        ###\033[0m        ####   ##')
+    print('                                  ###   ###')
+    print('                                ####   ###')
+    print('           ####          ##########   ####')
+    print('           #######################   ####')
+    print('            ####################   ####')
+    print('              ##################  ####')
+    print('                ############      ##')
+    print('                   ########        ###')
+    print('                  #########        #####')
+    print('                ############      ######')
+    print('               ########      #########')
+    print('                 #####       ########')
+    print('                   ###       #########')
+    print('                  ######    ############')
+    print('                 #######################')
+    print('                 #   #   ###  #   #   ##')
+    print('                 ########################')
+    print('                  ##     ##   ##     ##')
+    print('')
+    print('')
+    print(' \033[38;2;255;165;0m                Thanks For Using DVHACK\033[0m')
+    print('')
+    print('')
+    print('')
+
+END = '\033[0m'
+
+if Banner == 7:
+    print(RED + r'''
+        IIIIII    dTb.dTb        _.---._
+          II     4'  v  'B   .'"".'/|\`.""'.
+          II     6.     .P  :  .' / | \ `.  :
+          II     'T;. .;P'  '.'  /  |  \  `.'
+          II      'T; ;P'    `. /   |   \ .'
+        IIIIII     'YvP'       `-.__|__.-'
+
+                I love shell --me ''' + END)
+    print('')
+    print('')
+
+
+
+
+                                                                                                                                                                                      
+
+                                                                                                                
+print("              \033[31mDVHack \033[38;2;255;165;0mV1\033[0m                \033[31m To exit enter: \033[38;2;255;165;0m Exit\033[0m                  \033[31mCreator:\033[38;2;255;165;0m  DaddyV\033[0m")
+print('')
+print('')
+print('')
+print("\033[35m               ┌────────────────────────────────────────────┐\033[0m")
+print("\033[35m               │\033[0m  \033[36m01 ► ZPhisher\033[0m                           \033[35m  │\033[0m")
+print("\033[35m               │\033[0m  \033[36m02 ► Metasploit Console (RAT CONSOLE)\033[0m    \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m03 ► MSFVenom (RAT PAYLOAD)\033[0m              \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m04 ► NIKTO (Web Scanner)\033[0m                 \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m05 ► NMAP (Network Scanner)\033[0m              \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m06 ► Wireshark\033[0m                           \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m07 ► John The Ripper (Password Tool)\033[0m      \033[35m│\033[0m")
+print("\033[35m               │\033[0m  \033[36m08 ► SQLMAP (SQL Tool)\033[0m                   \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m09 ► Netcat\033[0m                              \033[35m │\033[0m")
+print("\033[35m               │\033[0m  \033[36m10 ► SEToolKit\033[0m                           \033[35m │\033[0m")
+print("\033[35m               └────────────────────────────────────────────┘\033[0m")
+
+def enter_option():
+    print("\033[31m")
+    print("╭─[ OPTION ]")
+    print("╰──➤$ ", end="")
+    Option = input()
+    print("\033[0m")
+
+    return Option
+
+
+Option = enter_option()
+
+
+if Option == "01":
+    subprocess.run("cd ZPhisher/zphisher && bash zphisher.sh", shell=True)
+
+if Option == '02':
+    subprocess.run('msfconsole')
+
+
+if Option == '03':
+    subprocess.run('msfvenom')
+
+
+if Option == '04':
+    subprocess.run('nikto')
+
+
+if Option == '05':
+    subprocess.run('nmap')
+
+
+if Option == '06':
+    subprocess.Popen(['wireshark'])
+
+if Option == '07':
+    subprocess.run(['john', '--help'])
+
+
+if Option == '08':
+    subprocess.run('sqlmap')
+
+
+if Option == '09':
+    subprocess.run(['nc', '-h'])
+
+if Option == '10':
+    subprocess.run(['sudo', 'setoolkit'])
+
+if Option == 'Exit':
+    sys.exit()
+
+
+
+
+
